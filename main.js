795672d454230c5052656b44e6060bf3d711e81a
soh226 <85522920+soh226@users.noreply.github.com>
2022-03-06 09:39:41 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+object=[];
+img="";
+status="";
+function preload(){
+    img=loadImage("TVlamp.png");
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById('status').innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error , results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+objects = results;
+}
+function draw(){
+    image(img,0,0,640,420);
+}
